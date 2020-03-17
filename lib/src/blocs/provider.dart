4bec3b44dd2b5b17ee2e6f1df46740e45d955dbf
tbnicolas <tnicolas@uninorte.edu.co>
2020-03-17 13:51:44 -0500

    Primer Commit
    -Falta contralar errores en el login y signUP
    -En cuanto a vistas contiene Login, SignUp, HomeView, Chat, DoctorProfileView y DoctorSearch

---
+++ lib/src/blocs/provider.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:medigestion/src/blocs/login_bloc.dart';
+export 'login_bloc.dart';
+
+class Provider extends InheritedWidget {
+  final loginBloc = new LoginBloc();
+  static Provider _instancia;
+//Modelo Singleton
+  factory Provider({Key key, Widget child}) {
+    //Si la instancia no esta creada, se crea
+    //caso contrario se regresa la misma
+    if (_instancia == null) {
+      _instancia = new Provider._internal(
+        key: key,
+        child: child,
+      );
+    }
+    return _instancia;
+  }
+  Provider._internal({Key key, Widget child}) : super(key: key, child: child);
+
+  //Key= identificador unico del child
+  //Provider({Key key, Widget child})
+  //    :super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) => true;
+
+  static LoginBloc of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<Provider>().loginBloc;
+  }
+}
---
+++ lib/src/providers/firebaseUser_provider.dart
@@ -0,0 +1,91 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+class FirebaseUserProvider {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final Firestore _db = Firestore.instance;
+ /*  String grou
+  FirebaseUserProvider.dataMessage(); */
+
+//SignInWithCredentials
+  Future<AuthResult> signInWithCredentials(
+      String email, String password) async {
+    return await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+//signUp
+  Future<AuthResult> signUp(String email, String password) async {
+    return await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+//Está logueado
+  Future<bool> isSignedIn() async {
+    final currentUser = await _firebaseAuth.currentUser();
+    return currentUser != null;
+  }
+
+  Stream<String> get authState {
+    return _firebaseAuth.onAuthStateChanged.map((user) => user?.uid);
+  }
+
+//Obtener informacion del usuario
+  Future<FirebaseUser> getUser() async {
+    final user = await _firebaseAuth.currentUser();
+    return user;
+  }
+
+//Salir de la session
+  Future<void> signOut() async {
+    return await _firebaseAuth.signOut();
+  }
+
+//Añadir usuario a CloudFireStore
+  Future updateUserData(FirebaseUser user) async{
+    DocumentReference ref = _db.collection('users').document(user.uid);
+    return await ref.setData({
+      'uid' : user.uid,
+      'email' : user.email,
+      'lastSeen' : DateTime.now(),
+    }, merge: true);
+  }
+
+  
+
+//Que hacer, cuando le da al boton de enviar
+  Future onSendMessage({@required String content,@required int type,@required String groupChatId,@required String id,@required String peerId}) async{
+    DocumentReference documentReference = _db
+            .collection('messages')
+            .document(groupChatId)
+            .collection(groupChatId)
+            .document(DateTime.now().millisecondsSinceEpoch.toString());
+
+    _db.runTransaction((transaction) async{
+       await transaction.set(
+          documentReference,
+          {
+            'idFrom': id,
+            'idTo': peerId,
+            'timestamp': DateTime.now().millisecondsSinceEpoch.toString(),
+            'content': content,
+            'type': type
+          },
+        );
+    });
+  }
+
+//Añadir usuarios que estan chateando con un doctor
+Future updateDoctorListChat(String doctorID, String patientID, String patientEmail) async{
+    DocumentReference ref = _db.collection('doctors').document(doctorID).collection('chattingWith').document(patientID);
+    return await ref.setData({
+      'email': patientEmail,
+      'patient' : patientID,
+    }, merge: true);
+  }
+
+
+  /* Stream<QuerySnapshot> get dataMessage(){
+    return  _db.collection('messages').document(groupChatId).collection(groupChatId).orderBy('timestamp', descending: true).limit(20).snapshots();
+  } */
+}
